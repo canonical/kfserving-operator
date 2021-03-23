423572b4ed15f6cc4763e89dcb09ea6e7ec1d29c
Kenneth Koski <kenneth.koski@canonical.com>
2021-03-23 14:15:50 -0500

    Fix CI/CD

--- src/charm.py
+++ src/charm.py
@@ -132,6 +132,7 @@ class Operator(CharmBase):
                 ],
             }
         ]
+
         validating = [
             {
                 "name": "inferenceservice.serving.kubeflow.org",
